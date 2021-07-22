4fb9ae52cc6ebc450da983643fcbd7f7f0898edf
2528844366qiqi <2528844366@qq.com>
2021-07-22 10:17:10 +0800

    登录页面开发、验证码接口、注册接口、登录的接口的调试

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -8,8 +8,8 @@
             <!-- 表单 -->
             <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="login-form" size="medium">
                 <el-form-item prop="username" class="item-from">
-                    <label for="">邮箱</label>
-                    <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
+                    <label for="username">邮箱</label>
+                    <el-input id="username" type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
                 </el-form-item>
                 <el-form-item prop="password" class="item-from">
                     <label for="">密码</label>
@@ -26,25 +26,27 @@
                             <el-input v-model.number="ruleForm.code" minlength="6" maxlength="6"></el-input>
                         </el-col>
                         <el-col :span="9">
-                            <el-button type="success" class="block">获取验证码</el-button>
+                            <el-button type="success" class="block" @click="getSms()">获取验证码</el-button>
                         </el-col>
                     </el-row>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="danger" @click="submitForm('ruleForm')" class="login-btn block">提交</el-button>
+                    <el-button type="danger" @click="submitForm('ruleForm')" class="login-btn block" :disabled="loginButtonStatus">{{model === 'login' ? '登录' : '注册'}}</el-button>
                 </el-form-item>
             </el-form>
         </div>
     </div>
 </template>
 <script>
-
+import {GetSms} from '@/api/login'
+import {onMounted, reactive, ref} from '@vue/composition-api';
 import { stripscript } from '@/utils/validate.js';
+import axios from 'axios';
 export default {
     name:"login",
-    data(){
-      // 验证用户名s
-      var validateUsername = (rule, value, callback) => {
+    setup(props, {refs, root}){
+        // 验证用户名s
+      let validateUsername = (rule, value, callback) => {
         let reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
         if (value === '') {
           callback(new Error('请输入用户名'));
@@ -55,7 +57,7 @@ export default {
         }
       };
       // 验证密码
-      var validatePassword = (rule, value, callback) => {
+      let validatePassword = (rule, value, callback) => {
         let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/
         if (value === '') {
           callback(new Error('请输入密码'));
@@ -66,21 +68,21 @@ export default {
         }
       };
       // 验证重置密码
-      var validatePasswords = (rule, value, callback) => {
-        if(this.model == 'login'){
+      let validatePasswords = (rule, value, callback) => {
+        if(model.value == 'login'){
             callback();
         }
         let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/
         if (value === '') {
           callback(new Error('请再次输入密码'));
-        } else if (value != this.ruleForm.password) {
+        } else if (value != ruleForm.password) {
           callback(new Error('两次密码不正确'));
         } else {
           callback();
         }
       };
       // 验证验证码
-      var validateCode = (rule, value, callback) => {
+      let validateCode = (rule, value, callback) => {
         let reg = /^[a-z0-9]{6}$/
         if (value === '') {
           return callback(new Error('请输入验证码'));
@@ -90,60 +92,92 @@ export default {
           callback();
         }
       };
-        return {
-            menuTab:[
+        //这里面放置data数据、生命周期、自定义的函数
+        const menuTab = reactive([
                 {txt:'登录', current:true, type:'login'},
                 {txt:'注册', current:false, type:'register'}
-            ],
-            // 模块值
-            model:'login',
-
-            ruleForm: {
+            ])
+          
+          const loginButtonStatus = ref(false)
+           const ruleForm = reactive({
                 username: '',
                 password: '',
                 passwords: '',
                 code: ''
-            },
-        rules: {
-          username: [
-            { validator: validateUsername, trigger: 'blur' }
-          ],
-          password: [
-            { validator: validatePassword, trigger: 'blur' }
-          ],
-          passwords:[
-            { validator: validatePasswords, trigger: 'blur' }
-          ],
-          code: [
-            { validator: validateCode, trigger: 'blur' }
-          ]
-        }
-    }
-    },
-    created(){},
-    mounted(){},
-    // 写函数的地方
-    methods:{
-        toggleMneu(data){
-            this.menuTab.forEach(elem => {
+            })
+
+            const rules = reactive({
+                username: [
+                    { validator: validateUsername, trigger: 'blur' }
+                ],
+                password: [
+                    { validator: validatePassword, trigger: 'blur' }
+                ],
+                passwords:[
+                    { validator: validatePasswords, trigger: 'blur' }
+                ],
+                code: [
+                    { validator: validateCode, trigger: 'blur' }
+                ]
+            })
+        // 声明函数
+        const toggleMneu = (data => {
+            console.log(data)
+            menuTab.forEach((elem,index) => {
                 elem.current = false
             });
-            data.current = true,
-            this.model = data.type
-        },
-        submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
+            data.current = true
+            model.value = data.type
+        })
+        // 获取验证码
+        const getSms = (() => {
+          // 进行提示
+          if(ruleForm.username == ''){
+            root.$message.error('邮箱不能为空！！');
+            return false
+          }
+
+          GetSms({username:ruleForm.username, module:'login'}).then(response => {
+            console.log(response)
+          }).catch(error => {
+            console.log(error)
+          })
+        })
+
+        const model = ref('login')
+        const submitForm = (formName => {
+            refs[formName].validate((valid) => {
           if (valid) {
             alert('submit!');
           } else {
             console.log('error submit!!');
             return false;
           }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
+            })
+        })
+
+        // 生命周期挂载完成
+        onMounted(() => {
+          // GetSms()
+        })
+        return {
+            menuTab,
+            model,
+            toggleMneu,
+            submitForm,
+            getSms,
+            ruleForm,
+            rules,
+            loginButtonStatus
+        }
+    },
+    created(){},
+    mounted(){},
+    // 写函数的地方
+    methods:{
+    //   resetForm(formName) {
+    //     this.$refs[formName].resetFields();
+    //   }
     }
 }
 </script>
